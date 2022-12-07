fc0985ca836b83fae3bc7db9ef0efc7590b15398
Addison Severnak <addisonsevernak@gmail.com>
2022-12-06 19:36:38 -0600

    added events

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import './less/index.less'
 
 // Your code goes here!
+
+let funBusLogo = document.querySelector('.logo-heading');
+funBusLogo.addEventListener('mouseover', event => {
+    event.target.style.backgroundColor = 'green';
+});
+
+let funBusIntro = document.querySelector('.intro');
+funBusIntro.addEventListener('dblclick', event => {
+    alert('YOU HAVE CLICKED!');
+});
+
+let letsGoImg = document.querySelector('.img-content');
+letsGoImg.addEventListener('keydown', escKey);
+function escKey(event) {
+    if(event.key === 'Escape') {
+        [img-content].classList.add('off');
+    }
+}
